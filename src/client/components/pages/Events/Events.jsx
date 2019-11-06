7dd1e757a1b2399f35f7a43adf55c303dd313d3f
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-11-05 23:21:06 -0500

    Add events route

---
+++ src/client/components/pages/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Events extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the Events page.</h1>
+      </div>
+    );
+  }
+}
+
+export default Events;
